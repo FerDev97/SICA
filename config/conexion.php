dd6c85dfdcf6716f983683ebfd68ced99e851c02
FerDev97 <fer.aravalo1997@gmail.com>
2018-08-29 11:24:10 -0500

    Cambio Conexion

--- config/conexion.php
+++ config/conexion.php
@@ -1,5 +1,6 @@
 <?php
-$conexion = new mysqli('localhost', 'root', 'sica', '');
+$conexion = new mysqli('localhost', 'root', '', 'sica');
 if ($conexion->connect_errno) {
     echo "Error de conexion";
 }
+?>
