1162013e7f0243b378963632a0075f5e9e945703
JessiRosales <jessirosales2@gmail.com>
2018-08-31 14:10:40 -0600

    conexion

--- config/conexion.php
+++ config/conexion.php
@@ -1,9 +1,5 @@
 <?php
-<<<<<<< HEAD
-$conexion = new mysqli('localhost', 'root', '', 'sicanew');
-=======
 $conexion = new mysqli('localhost', 'root', '', 'sica');
->>>>>>> 7c066d4c24d352607ca8311fd2dbc2ee3a465091
 if ($conexion->connect_errno) {
     echo "Error de conexion";
 }
