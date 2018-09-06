33534d65e961d34442539d0a1935c8e2b0e666f0
FerDev97 <fer.aravalo1997@gmail.com>
2018-09-06 15:23:24 -0500

    Docente combo solo activos.

--- pages/materias.php
+++ pages/materias.php
@@ -151,7 +151,7 @@ error_reporting(E_ALL & ~E_NOTICE);
                               <option value="">Seleccione Docente</option>
                                <?php
                       include '../config/conexion.php';
-                      $result = $conexion->query("select p.eid_personal as id, p.cnombre as nombre, p.capellido as apellido from tpersonal as p, tcargos as c where p.efk_idcargo=c.eid_cargo and c.ccargo='Docente' and iestado='1'");
+                      $result = $conexion->query("select p.eid_personal as id, p.cnombre as nombre, p.capellido as apellido from tpersonal as p, tcargos as c where p.efk_idcargo=c.eid_cargo and c.ccargo='Docente' and p.iestado='1'");
                       if ($result) {
 
                         while ($fila = $result->fetch_object()) {
