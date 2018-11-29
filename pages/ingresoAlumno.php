1105d76649417a87ba6b52d594dff16921cd23d8
KevinJovelDeveloper <kevinjovel9@gmail.com>
2018-11-28 22:43:53 -0600

    consulta

--- pages/ingresoAlumno.php
+++ pages/ingresoAlumno.php
@@ -346,7 +346,7 @@ function go(){
       <option value="Seleccione Opcion" selected hidden>Seleccione Opcion</option>
       <?php  
       include "../config/conexion.php";
-      $result = $conexion->query("SELECT tgrado.cgrado, tbachilleratos.cnombe, tsecciones.cseccion,tbachilleratos.eestado,topciones.eid_opcion FROM topciones INNER JOIN tbachilleratos ON topciones.efk_bto = tbachilleratos.eid_bachillerato INNER JOIN tgrado ON topciones.efk_grado = tgrado.eid_grado INNER JOIN tsecciones ON topciones.efk_seccion = tsecciones.eid_seccion WHERE tbachilleratos.eestado=1 order by tbachilleratos.cnombe");
+      $result = $conexion->query("SELECT topciones.eid_opcion,tgrado.cgrado,tbachilleratos.cnombe,tsecciones.cseccion,topciones.efk_seccion,topciones.eestado FROM topciones INNER JOIN tgrado ON topciones.efk_grado = tgrado.eid_grado INNER JOIN tbachilleratos ON topciones.efk_bto = tbachilleratos.eid_bachillerato INNER JOIN tsecciones ON topciones.efk_seccion = tsecciones.eid_seccion WHERE topciones.eestado=1 order by tbachilleratos.cnombe");
       if ($result) {
           while ($fila = $result->fetch_object()) {
            echo "<option value=".$fila->eid_opcion.">".$fila->cgrado."° ".$fila->cnombe." ".$fila->cseccion."</option>";
