293825be5341b01ca300d088b4b24b8ead1f2650
KevinJovel <kevinjovel9@gmail.com>
2019-01-14 11:54:27 -0600

    gestion de inscripcion

--- pages/menu.php
+++ pages/menu.php
@@ -26,7 +26,7 @@ if($result)
                   <ul class="nav nav-list tree">
                   <li><a href="fanio.php">Año Escolar Activo</a></li>
                      <li><a href="permisos.php">Permisos Temporales</a></li>
-                     <li><a href="cadministradores.php">Lista de administradores</a></li>
+                     <!-- <li><a href="cadministradores.php">Lista de administradores</a></li> -->
                    
               
                  </ul>
@@ -36,7 +36,7 @@ if($result)
                     <span class="fa-angle-right fa right-arrow text-right"></span>
                   </a>
                   <ul class="nav nav-list tree">
-                      <li><a href="ingresoAlumno.php">Gestionar proceso</a></li>
+                      <li><a href="gestionarInscripcion.php">Gestionar proceso</a></li>
                       <?php 
                       if($clausurado==0){
                       ?>
@@ -67,7 +67,7 @@ if($result)
                       <?php 
                       if($clausurado==0){
                       ?>
-                      <li><a href="listabus.php">Gestion de Periodos</a></li>
+                      <li><a href="fperiodo.php">Gestion de Periodos</a></li>
                         <li><a href="cnotas.php">Registro de Notas</a></li>
                        
                         <li><a href="listabus.php">Boleta de notas</a></li>
